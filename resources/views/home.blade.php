f54b0a4d5cc6c1a502720d9255ee3d0d4af85320
g1ammi93 <giammariacarelli93@hotmail.it>
2024-03-07 17:50:15 +0000

    Added Img

---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DC Comics</title>
+
+
+    <link rel="icon" href="{{ asset('images/dc-logo.png') }}" type="images/png">
+</head>
+
+
+<body>
+
+
+</body>
+
+</html>
